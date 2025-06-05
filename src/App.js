0048f83256edc9e351b561c2d9231ddfa04bc6ad
Shraddharai412 <shraddharai412@gmail.com>
2025-06-05 23:24:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import ContactManager from './Components/ContactManager';
+function App()
+{
+  return(
+<div  style={{marginTop:'10%'}}>
+     <ContactManager/>
+   </div>
+  )
+}
+export default  App;
+
+
+
+
+
